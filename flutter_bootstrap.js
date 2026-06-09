e17eb5ad7172bf35770f5d146c85bc88d701ee9b
hunghd <hunghd.yb@gmail.com>
2026-06-09 09:37:51 +0700

    Deploy dev v0.0.1 build 1 (2026-06-09T02:37:51Z)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1187112527" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3407946761" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
