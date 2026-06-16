160f21db9664ed6ca3ab712037da87836909192d
hunghd <hunghd.yb@gmail.com>
2026-06-16 10:27:37 +0700

    Deploy dev v0.0.13 build 13 (2026-06-16T03:27:36Z)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=12"},{}]};
+_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=13"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3191309915" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2250606204" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
